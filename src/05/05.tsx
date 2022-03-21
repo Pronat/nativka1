26a2a389258afbb915d48ca58b654df5e3ca8602
AlexPron <Fotolinza@gmail.com>
2022-03-21 22:44:33 +0300

    nativka4-map-1.2

---
+++ src/05/05.tsx
@@ -0,0 +1,39 @@
+type ManType = {
+    name: string;
+    age: number;
+}
+
+const people: Array<ManType> = [
+    {name: "Andrew Ivanov", age: 33},
+    {name: "Alexandr Petrov", age: 24},
+    {name: "Dmitry Sidorov", age: 18},
+]
+
+const dimychTransformator = (man: ManType) => {
+    return {
+        stack: ["css, html", "js", "tdd", "react"],
+        firstName: man.name.split(" ")[0],
+        lastName: man.name.split(" ")[1],
+    }
+}
+
+    const devs = [
+        {
+            stack: ["css, html", "js", "tdd", "react"],
+            firstName: "Andrew", lastName: "Ivanov"
+        },
+        {
+            stack: ["css, html", "js", "tdd", "react"],
+            firstName: "Alexander", lastName: "Petrov"
+        },
+        {
+            stack: ["css, html", "js", "tdd", "react"],
+            firstName: "Dmitry", lastName: "Sidorov"
+        },
+    ]
+
+const dev2 = [
+    dimychTransformator(people[0]),
+    dimychTransformator(people[1]),
+    dimychTransformator(people[2])
+]
