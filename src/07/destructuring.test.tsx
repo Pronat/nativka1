883bc384651f80231c3882e2d7da932088bfe05b
AlexPron <Fotolinza@gmail.com>
2022-04-01 22:28:48 +0300

    nativka6-distrPrisv-1.3

--- src/07/destructuring.test.tsx
+++ src/07/destructuring.test.tsx
@@ -1,5 +1,7 @@
-test("", () => {
-    let props = {
+let props: ManType;
+
+beforeEach(() => {
+    props = {
         name: "Dimych",
         age: 32,
         lessons: [{title: "1"}, {title: "2"}],
@@ -9,17 +11,33 @@ test("", () => {
             }
         }
     }
+})
 
+type LessonType = {
+    title: string
+}
+
+type ManType = {
+    name: string,
+    age: number,
+    lessons: Array<LessonType>,
+    address: {
+        street: {
+            title: string
+        }
+    }
+}
+
+
+
+test("", () => {
     // const age = props.age;
     // const lessons = props.lessons;
-    const {age, lessons} = props;
-
-    const a = props.age;
-    const l = props.lessons;
+    var {age, lessons} = props;
+    const {title} = props.address.street
 
     expect(age).toBe(32);
     expect(lessons.length).toBe(2);
 
-    expect(a).toBe(32);
-    expect(l.length).toBe(2);
+    expect(title).toBe("Nezavisimisti Street")
 })
