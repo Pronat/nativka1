c346b9e45e078db004f14035ef0e9407e514fa0d
Pronat <Fotolinza@gmail.com>
2022-05-22 19:10:50 +0300

    nativka10-6

--- src/10/10.test.tsx
+++ src/10/10.test.tsx
@@ -1,30 +1,46 @@
-import {UserType} from "./10";
+import {makeHairStyle, moveUser, UserType, UserWithLaptopType} from "./10";
 
-function makeHairStyle(u: UserType, power: number) {
 
-    const copy = {
-        ...u,
-        hair: u.hair / power,
-
-        // copy.hair = u.hair / power
-    }
-    return copy
-}
 
 test('reference type test', ()=> {
     let user: UserType = {
         name: 'Dimych',
         hair: 32,
         address: {
-            title: 'Minsk'
+            city: 'Minsk'
         }
     }
 
     const awesomeUser = makeHairStyle(user, 2)
-    user.address.title = 'Kiev'
+    user = awesomeUser
 
 
     expect(user.hair).toBe(32)
     expect(awesomeUser.hair).toBe(16)
     expect(awesomeUser.address).toBe(user.address)
+})
+
+test('change address', ()=> {
+    let user: UserWithLaptopType = {
+        name: 'Dimych',
+        hair: 32,
+        address: {
+            city: 'Minsk',
+            house: 12
+        },
+        laptop: {
+            title: 'ZenBook'
+        }
+    }
+
+
+    const movedUser = moveUser(user, 2)
+    user = movedUser
+
+
+    expect(user).not.toBe(movedUser)
+    expect(user.address).not.toBe(movedUser.address)
+    expect(user.laptop).not.toBe(movedUser.laptop)
+    expect(user.address.title).toBe('Kiev')
+
 })
--- src/10/10.tsx
+++ src/10/10.tsx
@@ -1,5 +1,22 @@
 export type UserType = {
     name: string
     hair: number
-    address: { title: string }
+    address: { city: string, house?: number }
+}
+
+export type LaptopType = {
+    title: string
+}
+export type UserWithLaptopType = UserType & {
+    laptop: LaptopType
+}
+
+export function moveUser(u: UserWithLaptopType, city: string) {
+
+    const copy = {
+        ...u,
+
+    }
+    copy.address = {...copy.address, city: city}
+    return copy
 }
