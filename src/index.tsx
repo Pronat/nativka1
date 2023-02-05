d8174bb2c336441cf978a8e23f08df686693fbad
AlexPron <Fotolinza@gmail.com>
2023-02-05 22:18:53 +0300

    nativka1-2

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {sum} from "./03/03";
-import {User} from "./06/06";
+import {value} from "./01-hello-tests/01";
 
-var a = sum;
 
-ReactDOM.render(<User />, document.getElementById('root')
+const a = value
+
+
+
+ReactDOM.render(<App />, document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
