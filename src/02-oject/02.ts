b1b952caccf1ec86b9547164b604cba7dfbe1c0e
AlexPron <Fotolinza@gmail.com>
2023-02-06 22:14:19 +0300

    nativka2-2

--- src/02-oject/02.ts
+++ src/02-oject/02.ts
@@ -8,7 +8,22 @@ export const student = {
             title: "Minsk",
             countryTitle: "Belarus"
         }
-    }
+    },
+    technologies: [
+        {
+            id: 1,
+            title: "HTML"
+        },
+        {
+            id: 2,
+            title: "CSS"
+        },
+        {
+            id: 3,
+            title: "React"
+        },
+    ]
 }
 
 console.log(student.address.streetTitle)
+console.log(student.technologies[2].title)
