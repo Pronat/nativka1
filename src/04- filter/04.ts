9e5c33ff22533d0514221647ae230484361d08c9
AlexPron <Fotolinza@gmail.com>
2023-02-12 20:49:51 +0300

    nativkaFilter-7

--- src/04- filter/04.ts
+++ src/04- filter/04.ts
@@ -1,5 +1,18 @@
 export const ages = [18, 24, 100, 5, 1, 53, 76, 78]
+const oldAges = (age: number) => {
+    return ages.filter((el) => el > 99)
 
+}
 
-const oldAges = ages.filter((el) => el > 99)
-console.log(oldAges)
+export type CoursesType =
+    { title: string, price: number }
+
+const courses = [
+    {title: 'CSS', price: 120},
+    {title: 'HTML', price: 70},
+    {title: 'React', price: 200},
+]
+const coursesCheap = (course: CoursesType) => {
+    return courses.filter((el) => el.price < 100)
+}
+
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import {Filter} from "./04- filter/04";
 
 function App() {
   return (
