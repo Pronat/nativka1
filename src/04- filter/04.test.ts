7af875e90d7c2c52ad7610006452231e7c1eb545
AlexPron <Fotolinza@gmail.com>
2023-02-12 20:34:38 +0300

    nativkaFilter-4

--- src/04- filter/04.test.ts
+++ src/04- filter/04.test.ts
@@ -1,10 +1,16 @@
 export const ages = [18, 24, 100, 5, 1, 53, 76, 78]
-const oldAges = ages.filter((el) => el > 99)
+const oldAges = () => {
+   const a = ages.filter((el) => el > 99)
+    return a
+}
 
 const courses = [
     {title: 'CSS', price: 120},
     {title: 'HTML', price: 70},
     {title: 'React', price: 200},
 ]
-
+const coursesCheap = () => {
+    let c = courses.filter((el) => el.price < 100)
+    return c
+}
 
