b732022d716607af2ba0d4882022afc9e2b1de7b
AlexPron <Fotolinza@gmail.com>
2023-02-12 21:06:52 +0300

    nativkaFilter-10

--- src/04- filter/04.test.ts
+++ src/04- filter/04.test.ts
@@ -1,14 +1,19 @@
 import {ages, oldAges} from "./04";
 
-test('check age mpre 99', ()=> {
+test('check age more 99', ()=> {
     const ages = [18, 24, 100, 5, 1, 53, 76, 78]
 
     const oldAges2 = ages.filter((el) => el > 99)
     expect(oldAges2.length).toBe(1)
 })
-test('check age mpre 99', ()=> {
-    const ages = [18, 24, 100, 5, 1, 53, 76, 78]
+test('check price less 99', ()=> {
+    const courses = [
+        {title: 'CSS', price: 120},
+        {title: 'HTML', price: 70},
+        {title: 'React', price: 200},
+    ]
 
-    const oldAges2 = ages.filter((el) => el > 99)
-    expect(oldAges2.length).toBe(1)
+    const courses2 = courses.filter((el) => el.price < 100)
+    expect(courses2.length).toBe(1)
+    expect(courses2[0].price).toBe(70)
 })
